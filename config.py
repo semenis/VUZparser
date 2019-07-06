ce3b4865351f6ffbbefec8f95c3201dbce5df9fe
Семен <semen.say@ya.ru>
2019-07-07 00:38:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 name = 'Иванов Иван Иванович'
 
-spbgu_links = ['https://cabinet.spbu.ru/Lists/1k_EntryLists/list_98a59a22-e0f0-48b2-952b-0733d18331c1.html', 'https://cabinet.spbu.ru/Lists/1k_EntryLists/list_30f6f697-fab8-4883-be4a-3bfb2347c847.html', 'https://cabinet.spbu.ru/Lists/1k_EntryLists/list_c4670621-d97d-44a5-b3b8-0ef97a3c98f8.html']
+
+spbgu_links = ['https://cabinet.spbu.ru/Lists/1k_EntryLists/list_98a59a22-e0f0-48b2-952b-0733d18331c1.html', 'https://cabinet.spbu.ru/Lists/1k_EntryLists/list_a26329cf-20a7-4899-9f25-0c4ef0d1dcaa.html', 'https://cabinet.spbu.ru/Lists/1k_EntryLists/list_c4670621-d97d-44a5-b3b8-0ef97a3c98f8.html']
 #Ссылки для примера, взяты из https://cabinet.spbu.ru/Lists/1k_EntryLists/index_comp_groups.html
+
+miet_links = ['https://abit.miet.ru/lists/cache/data/01.03.04-o-b.json','https://abit.miet.ru/lists/cache/data/27.03.04-o-b.json','https://abit.miet.ru/lists/cache/data/09.03.01-o-b.json']
+#Намемто номера направления установите желаемое
