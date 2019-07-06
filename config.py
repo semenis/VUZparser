3c80d7642e3961c96abe03e9cb1d70df4e01006f
Семен <semen.say@ya.ru>
2019-07-06 22:39:32 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+name = 'Иванов Иван Иванович'
+
+spbgu_links = ['https://cabinet.spbu.ru/Lists/1k_EntryLists/list_98a59a22-e0f0-48b2-952b-0733d18331c1.html', 'https://cabinet.spbu.ru/Lists/1k_EntryLists/list_30f6f697-fab8-4883-be4a-3bfb2347c847.html', 'https://cabinet.spbu.ru/Lists/1k_EntryLists/list_c4670621-d97d-44a5-b3b8-0ef97a3c98f8.html']
+#Ссылки для примера, взяты из https://cabinet.spbu.ru/Lists/1k_EntryLists/index_comp_groups.html
